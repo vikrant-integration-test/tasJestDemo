5d8447e5a3a9e76d1d9348592f6e3dca60ec7692
Himanshi Gupta <himanshigupta@lambdatest.com>
2021-10-11 16:30:03 +0530

    Added new tests

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -4,4 +4,8 @@ describe("Calculator tests", () => {
     test('adding 1 + 2 should return 3', () => {
         expect(mathOperations.sum(1, 2)).toBe(3);
     });
+
+    test('adding -1 + 2 should return 1', () => {
+        expect(mathOperations.sum(-1, 2)).toBe(1);
+    });
 })
