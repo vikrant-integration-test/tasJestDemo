739f454c56368f9816e4eb587bd14fd866fee4d5
Himanshi Gupta <himanshigupta@lambdatest.com>
2021-10-28 18:36:15 +0530

    New tests addition with suite

---
+++ src/calculatorWithSuite.test.js
@@ -0,0 +1,31 @@
+const getAboutUsLink = require("./calculator");
+
+describe('Test Language code', function () {
+    it("Returns about-us for english language", () => {
+        expect(getAboutUsLink("en-US")).toBe("/about-us");
+    });
+
+    it("Returns about-us for spanish language", () => {
+        expect(getAboutUsLink("es-ES")).toBe("/acerca-de");
+    });
+
+    it("Fail case for english language", () => {
+        expect(getAboutUsLink("en-Uws")).toBe("/about-us");
+    });
+
+    it("Fail case for spanish", () => {
+        expect(getAboutUsLink("es-ES")).toBe("/acerca-dedddd");
+    });
+
+    it("Returns meist for estonian language", () => {
+        expect(getAboutUsLink("et")).toBe("/meist");
+    });
+
+    it("Returns about-us for czech language", () => {
+        expect(getAboutUsLink("cs")).toBe("/o nás");
+    });
+
+    it("Fail czech language", () => {
+        expect(getAboutUsLink("cs")).toBe("/o nas");
+    });
+});
