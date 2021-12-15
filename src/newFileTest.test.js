0d3b6d5b703d85fa9231736ac533986839788895
Himanshi Gupta <himanshigupta@lambdatest.com>
2021-12-15 17:12:40 +0530

    Add few more tests

--- src/newFileTest.test.js
+++ src/newFileTest.test.js
@@ -34,5 +34,19 @@ describe("Comparision tests", () => {
         expect(result).toBe(false);
     });
 
- 
+    test('3<12 should return false', () => {
+        // arrange and act
+        var result = comparisionOperation.less(3, 12)
+
+        // assert
+        expect(result).toBe(true);
+    });
+
+    test('3<3 should return false', () => {
+        // arrange and act
+        var result = comparisionOperation.less(3, 3)
+
+        // assert
+        expect(result).toBe(false);
+    });
 })
