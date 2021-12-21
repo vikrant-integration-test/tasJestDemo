cfaed76fc111451059190b490f8a9590eb84ed79
Himanshi Gupta <himanshigupta@lambdatest.com>
2021-12-21 19:06:19 +0530

    add 2nd commit

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -3,7 +3,7 @@ test("Returns about-us for english language", () => {
     expect(getAboutUsLink("en-US")).toBe("/about-us");
 });
 
-test("Returns about-us for spanish language", () => {
+test("Return s about-us for spanish language", () => {
     expect(getAboutUsLink("es-ES")).toBe("/acerca-de");
 });
 
