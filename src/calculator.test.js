748c65bcc5c57cfae36caf9b2a7b12a116b5d747
Himanshi Gupta <himanshigupta@lambdatest.com>
2021-12-21 19:07:16 +0530

    add 4th commit

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,6 +1,6 @@
 const getAboutUsLink = require("./calculator");
 
-test("Returns about-us for english language", () => {
+test("Retuns about-us for english language", () => {
     expect(getAboutUsLink("en-US")).toBe("/about-us");
 });
 
