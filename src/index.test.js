74cac9b1a48ae126cbb3c434807b852c8f8ecd2f
Himanshi Gupta <89007259+himanshiLt@users.noreply.github.com>
2021-12-28 19:37:06 +0530

    Update test file (total 3 tests)

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,6 @@ const mathOperations = require('./index');
 
 describe("Calculator tests", () => {
     test('adding 1 + 2 should return 3', () => {
-        // arrange and act
         var result = mathOperations.sum(1, 2)
 
         // assert
