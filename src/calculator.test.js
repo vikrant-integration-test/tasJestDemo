4eeed2e8a5131dcf18b876e628dc45a0241eca27
Himanshi Gupta <himanshigupta@lambdatest.com>
2021-12-29 20:37:13 +0530

    add failure tests back

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -13,17 +13,17 @@ test.skip("Skip a test", () => {
     expect(getAboutUsLink("es-ES")).toBe("/acerca-de");
 });
 
-// test("Fail a test", () => {
-//     expect(getAboutUsLink("es-ES")).toBe("");
-// });
+test("Fail a test", () => {
+    expect(getAboutUsLink("es-ES")).toBe("");
+});
 
 // test("Blacklist a test", () => {
 //     expect(getAboutUsLink("es-ES")).toBe("");
 // });
 
-// test("Fail a test", () => {
-//     expect(getAboutUsLink("es-ES")).toBe("");
-// });
+test("Fail a test", () => {
+    expect(getAboutUsLink("es-ES")).toBe("");
+});
 
 test("Account added by arjun-rathi", () => {
     expect(getAboutUsLink("es-ES")).toBe("/acerca-de");
