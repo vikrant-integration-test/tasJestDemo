2fdb466cd2b504f3fe1619c1af6564583cdedb08
Himanshi Gupta <himanshigupta@lambdatest.com>
2021-12-30 20:06:25 +0530

    commit with multiple secret key

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -2,7 +2,7 @@ const getAboutUsLink = require("./calculator");
 
 
 test("Retuns about-us for english language", () => {
-    expect(getAboutUsLink("en-US")).toBe("/about-us");
+    expect(getAboutUsLink(process.env.ENGLISHCODE)).toBe("/about-us");
 });
 
 test("Returns about-us for spanish language", () => {
