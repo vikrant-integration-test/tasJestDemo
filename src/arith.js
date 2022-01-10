aa34bda8056d9cda9d1cc6d1953da30a1caf7b4a
Himanshi Gupta <89007259+himanshiLt@users.noreply.github.com>
2022-01-10 20:54:49 +0530

    Update arith.js

--- src/arith.js
+++ src/arith.js
@@ -2,6 +2,6 @@ const add = (a, b) => a + b;
 const mul = (a, b) => a * b;
 const sub = (a, b) => a - b;
 const div = (a, f) => a / f;
-const mod = (a, f) => a % f;
+const mod = (a, g) => a % g;
 
 module.exports = { add, mul, sub, div, mod };
