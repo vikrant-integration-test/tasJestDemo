0045192b31742cd60cd8b8aa1d330aa5a5c2ff05
Himanshi Gupta <89007259+himanshiLt@users.noreply.github.com>
2022-01-17 15:39:07 +0530

    n=4, 4th commit

--- src/comparision.test.js
+++ src/comparision.test.js
@@ -4,7 +4,6 @@ describe("Comparision tests", () => {
     test('3=3 should return true', () => {
         // arrange and act
         var result = comparisionOperation.equal(3, 3)
-
         // assert
         expect(result).toBe(true);
     });
