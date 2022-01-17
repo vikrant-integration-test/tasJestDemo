bd9c971f49ba12ac71b27d77ec110a3dde3fd110
Himanshi Gupta <himanshigupta@lambdatest.com>
2022-01-17 18:34:34 +0530

    1

--- src/arith.js
+++ src/arith.js
@@ -2,6 +2,6 @@ const add = (a, b) => a + b;
 const mul = (a, b) => a * b;
 const sub = (a, b) => a - b;
 const div = (a, b) => a / b;
-const mod = (a, g) => a % g;
+const mod = (a, b) => a % b;
 
 module.exports = { add, mul, sub, div, mod };
