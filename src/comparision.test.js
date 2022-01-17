209ac7a066d8b2f2a75e1ae1ad024a25091942d2
Himanshi Gupta <89007259+himanshiLt@users.noreply.github.com>
2022-01-17 18:30:54 +0530

    Update comparision.test.js

--- src/comparision.test.js
+++ src/comparision.test.js
@@ -2,7 +2,6 @@ const comparisionOperation = require('./comparision');
 
 describe("Comparision tests", () => {
     test('3=3 should return true', () => {
-        // arrange and act
         var result = comparisionOperation.equal(3, 3)
         // assert
         expect(result).toBe(true);
