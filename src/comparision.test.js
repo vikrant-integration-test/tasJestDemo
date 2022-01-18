50a47c7f7c350498b33a189148b81d42e9cf30e6
Himanshi Gupta <himanshigupta@lambdatest.com>
2022-01-18 13:01:37 +0530

    add more test

--- src/comparision.test.js
+++ src/comparision.test.js
@@ -69,4 +69,12 @@ describe("Comparision tests", () => {
         expect(result).toBe(true);
     });
 
+
+    test('5<=7 should return true', () => {
+        // arrange and act
+        var result = comparisionOperation.lessThanEqual(5, 7)
+
+        // assert
+        expect(result).toBe(true);
+    });
 })
