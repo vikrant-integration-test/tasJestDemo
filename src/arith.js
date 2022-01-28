ae1a47c72c1505642c301f448ca69dedb38a3854
Himanshi Gupta <himanshigupta@lambdatest.com>
2022-01-28 19:24:05 +0530

    add new method

--- src/arith.js
+++ src/arith.js
@@ -3,5 +3,6 @@ const mul = (a, b) => a * b;
 const sub = (a, b) => a - b;
 const div = (a, b) => a / b;
 const mod = (a, b) => a % b;
+const modu = (a, b) => a % b;
 
-module.exports = { add, mul, sub, div, mod };
+module.exports = { add, mul, sub, div, mod, modu };
