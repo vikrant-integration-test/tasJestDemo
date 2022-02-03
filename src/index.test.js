d6c0ccd7fca101a1d1ccb3a1ca4469f2bb1346a5
Himanshi Gupta <himanshigupta@lambdatest.com>
2022-02-03 15:59:59 +0530

    4th

--- src/index.test.js
+++ src/index.test.js
@@ -12,9 +12,7 @@ describe("Calculator tests", () => {
         var result = mathOperations.diff(3, 2)
         expect(result).toBe(1);
     });
-    
 
-    
     test('product 3 * 2 should return 6', () => {
         var result = mathOperations.product(3, 2)
 
