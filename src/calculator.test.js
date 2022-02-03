a305973c9fe08503ede9472c81799f601fa49cd8
Himanshi Gupta <himanshigupta@lambdatest.com>
2022-02-03 16:03:32 +0530

    4th, should not

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -14,9 +14,9 @@ test.skip("Skip a test", () => {
 });
 
 
-test("Fail a test -1", () => {
-    expect(getAboutUsLink("es-ES")).toBe("");
-});
+// test("Fail a test -1", () => {
+//     expect(getAboutUsLink("es-ES")).toBe("");
+// });
 
 // test("Blacklist a test", () => {
 //     expect(getAboutUsLink("es-ES")).toBe("");
